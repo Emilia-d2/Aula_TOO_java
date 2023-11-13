329ff623589ddb9b9ec0abbb172bf1cf4269be4f
Emilia <emilia.ferlin@sbsistemas.com.br>
2023-11-13 08:03:08 -0300

    feat:tudo

---
+++ mavenproject1/src/main/java/topico3/outras_classes/Questao1.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package topico3.outras_classes;
+
+import java.util.Collection;
+
+/**
+ *
+ * @author Mili
+ */
+
+/*
+1) Codifique a classe Questao1 no pacote br.edu.ifsul.bcc.too.avaliacao.primeiraetapa.
+O método extracaoInteiros deverá ser chamado, recebendo como parâmetro a
+referência para a variável json. Neste método, implemente uma rotina em java para
+extrair os valores (123, 0 e 59) de forma dinâmica. Converta-os para Integer,
+adicione-os em uma coleção e retorne. Em seguida implemente um método para
+imprimir na saída padrão. Esse método também deverá ser chamado no construtor.
+
+*/
+public class Questao1 {
+    public Questao1(String param){
+    }
+
+    private Collection<Integer> extracaoInteiros(String param){
+
+        return null;
+
+    }
+
+    public static void main(String[] args) {
+    String json= "[{codigo: 123}, {codigo: 0}, {codigo: 59}]";
+    }
+}
